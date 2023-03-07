7258d039e58138ccad08a457a086da3b7788f99a
Bänziger Thomas <thomas.baenziger@helvetia.ch>
2023-03-07 14:33:34 +0100

    vorbereitungen um übereinstimmende Nummern zu finden

--- src/ExcelStarter.java
+++ src/ExcelStarter.java
@@ -1,4 +1,4 @@
-import java.util.List;
+import java.util.*;
 
 public class ExcelStarter {
 	
@@ -11,6 +11,25 @@ public class ExcelStarter {
 		for(Nummerneinteilung k : listemitNummern) {
 			System.out.println(k);
 		}
+		
+//		find the most matching lists
+//		int MatchNumber12 = 0;
+//		int MatchNumber13 = 0;
+//		int MatchNumber34 = 0;
+		
+//		//loop the values from each row and compare them
+//		for(int i = 0; i < listemitNummern.size(); i++) {
+//			if(listemitNummern.get(i).getNumber1() == getNumber2()) {
+//				
+//				nummerneinteilung.getNummer1
+//				
+//				//Number 1 and Number 2 matching in first row
+//				MatchNumber12++;
+//			}
+//		}
+//	      
+//	      // Ergebnis ausgeben
+//	      System.out.println("Bestes Paar: " + bestesPaar);
+//	      System.out.println("Größe der Schnittmenge: " + maxGroesse);		
 	}
-
 }
